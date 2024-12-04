8205b6320657e1d22a3ffb10ad4594948ccb170d
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-12-04 23:16:30 +0600

    zod schema created for creatign new product

--- backend/src/app/modules/products/products.route.ts
+++ backend/src/app/modules/products/products.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { ProductControllers } from "./products.controller";
+import Auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
+import { ProductValidations } from "./products.validation";
+import ValidationRequest from "../../middlewares/zod_validation";
 
 const router: Router = Router();
 
 router.post('/',
+    Auth(UserRole.VENDOR),
+    ValidationRequest(ProductValidations.createProductValidationSchema),
     ProductControllers.createProduct)
 
 export const ProductRoutes = router;
