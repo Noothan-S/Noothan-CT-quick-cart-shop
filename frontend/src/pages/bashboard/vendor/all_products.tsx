23317fcfe461d967c361820ba39d46e67182c7a6
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-12-10 08:59:54 +0600

    all product page created for vendor dashboard and fetched vendord's products

--- frontend/src/pages/bashboard/vendor/all_products.tsx
+++ frontend/src/pages/bashboard/vendor/all_products.tsx
@@ -1,10 +1,35 @@
 import React from "react";
-import { useGetMyProfileQuery } from "../../../redux/features/user/profile.api";
+import { useGetAllProductsQuery } from "../../../redux/features/products/products.api";
+import { useAppSelector } from "../../../redux/hooks";
+import { useCurrentUser } from "../../../redux/features/auth/auth.slice";
+import { decrypt } from "../../../utils/text_encryption";
+import Loading from "../../../components/loading";
+import { Button, Result } from "antd";
+import { Link } from "react-router-dom";
 
 const AllProduct: React.FC = () => {
-  const { data: vendor } = useGetMyProfileQuery({});
+  const user = useAppSelector(useCurrentUser);
+  const { data, isLoading } = useGetAllProductsQuery({
+    vendorId: decrypt(user.vendor),
+  });
 
-  console.log(vendor);
+  if (isLoading) return <Loading />;
+  if (data.data.length > 1) {
+    return (
+      <Result
+        className="text-red-600"
+        title="No Item Found"
+        extra={
+          <Link to={"/dashboard/vendor/add-product"}>
+            {" "}
+            <Button type="primary" key="console">
+              Add One
+            </Button>
+          </Link>
+        }
+      />
+    );
+  }
 
   return <div>hello</div>;
 };
