002d843991b07c804174580d3933c3e44ed07f1b
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-12-10 09:07:11 +0600

    handle error

--- frontend/src/pages/bashboard/vendor/all_products.tsx
+++ frontend/src/pages/bashboard/vendor/all_products.tsx
@@ -9,12 +9,14 @@ import { Link } from "react-router-dom";
 
 const AllProduct: React.FC = () => {
   const user = useAppSelector(useCurrentUser);
-  const { data, isLoading } = useGetAllProductsQuery({
-    vendorId: decrypt(user.vendor),
-  });
+
+  const { data, isLoading } = useGetAllProductsQuery(
+    user?.vendor ? { vendorId: decrypt(user.vendor) } : {},
+    { skip: !user }
+  );
 
   if (isLoading) return <Loading />;
-  if (data.data.length > 1) {
+  if (data.data.length < 1) {
     return (
       <Result
         className="text-red-600"
