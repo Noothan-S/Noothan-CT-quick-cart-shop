4d43f6f7a7bc5d028212a9773d5a160c5fb25c29
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-12-11 09:25:23 +0600

    order table created for vendor

--- frontend/src/pages/dashboard/vendor/orders.tsx
+++ frontend/src/pages/dashboard/vendor/orders.tsx
@@ -1,5 +1,15 @@
+import { Result } from "antd";
+import VendorOrderTable from "../../../components/dashboard/vendors/vendor-order-table";
+import Loading from "../../../components/loading";
+import { useGetAllOrdersQuery } from "../../../redux/features/orders/order.api";
+
 const VendorOrders = () => {
-  return <div>hello</div>;
+  const { data: orders, isLoading, isError } = useGetAllOrdersQuery({});
+
+  if (isLoading) return <Loading />;
+  if (isError) return <Result status={"500"} />;
+
+  return <VendorOrderTable orders={orders.data} />;
 };
 
 export default VendorOrders;
