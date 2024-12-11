116203dd500d8432b0afea311bcfa76332e37e4b
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-12-12 00:30:08 +0600

    before review

--- frontend/src/pages/dashboard/admin/coupon.tsx
+++ frontend/src/pages/dashboard/admin/coupon.tsx
@@ -3,9 +3,12 @@ import CouponManagementTable from "../../../components/dashboard/coupon-manageme
 import Loading from "../../../components/loading.tsx";
 import { useGetAllCouponsQuery } from "../../../redux/features/coupon/coupon.api.ts";
 import { Plus } from "lucide-react";
+import { useState } from "react";
+import AssignNewCouponDrawer from "../../../components/dashboard/add-coupon-drawer.tsx";
 
 const AdminCoupons = () => {
   const { data: coupons, isLoading, isError } = useGetAllCouponsQuery({});
+  const [isOpenDrawer, setIsDrawerOpen] = useState<boolean>(false);
 
   if (isLoading) return <Loading />;
   if (isError) return <Result status={"500"} />;
@@ -28,7 +31,16 @@ const AdminCoupons = () => {
   return (
     <>
       <CouponManagementTable coupons={coupons.data} />
-      <FloatButton icon={<Plus />} tooltip={<div>Assign new coupon</div>} />
+      <FloatButton
+        onClick={() => setIsDrawerOpen(true)}
+        icon={<Plus />}
+        tooltip={<div>Assign new coupon</div>}
+      />
+
+      <AssignNewCouponDrawer
+        isOpenDrawer={isOpenDrawer}
+        setIsDrawerOpen={setIsDrawerOpen}
+      />
     </>
   );
 };
