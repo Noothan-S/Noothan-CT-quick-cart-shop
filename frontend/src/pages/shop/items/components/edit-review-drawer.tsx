8f42912bdff3755b9abe9835f25b1dcad56adc0b
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-12-16 17:33:54 +0600

    default values set for edit reviews drawer

--- frontend/src/pages/shop/items/components/edit-review-drawer.tsx
+++ frontend/src/pages/shop/items/components/edit-review-drawer.tsx
@@ -1,6 +1,9 @@
-import { Button, Drawer, Space } from "antd";
-import { Dispatch, FC, SetStateAction } from "react";
+import { Button, Drawer, Input, Rate, Space } from "antd";
+import { Dispatch, FC, SetStateAction, useState } from "react";
 import { IReview } from "../../../../interfaces/api.products.res.type";
+import { StarFilled } from "@ant-design/icons";
+import TextArea from "antd/es/input/TextArea";
+import { useForm } from "react-hook-form";
 
 interface IEditReviewDrawerProps {
   setReview: Dispatch<SetStateAction<null | IReview>>;
@@ -10,7 +13,18 @@ const EditReviewDrawer: FC<IEditReviewDrawerProps> = ({
   setReview,
   review,
 }) => {
-  console.log(review);
+  const [updatedRating, setUpdatedRating] = useState<number>(
+    review?.rating as number
+  );
+
+  const { handleSubmit, control } = useForm({
+    defaultValues: {
+      description: review?.description,
+      rating: updatedRating,
+    },
+  });
+
+  console.log(updatedRating);
   return (
     <>
       <Drawer
@@ -30,7 +44,22 @@ const EditReviewDrawer: FC<IEditReviewDrawerProps> = ({
           </Space>
         }
       >
-        helooo
+        <div className="flex flex-col items-center gap-4">
+          <TextArea
+            maxLength={200}
+            showCount
+            minLength={5}
+            rows={5}
+            size="large"
+          />
+          <Rate
+            value={updatedRating}
+            onChange={(val) => setUpdatedRating(val)}
+            character={<StarFilled />}
+            className="text-2xl text-red-500"
+          />
+          <p className="text-lg">Current Rating: {review?.rating} stars</p>
+        </div>
       </Drawer>
     </>
   );
