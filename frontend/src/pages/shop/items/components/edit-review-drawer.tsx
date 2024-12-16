8f2a48dfd0443c5ee5c4ed8861579f44d881dbea
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-12-16 17:39:45 +0600

    got form value for updating review by customer

--- frontend/src/pages/shop/items/components/edit-review-drawer.tsx
+++ frontend/src/pages/shop/items/components/edit-review-drawer.tsx
@@ -1,9 +1,9 @@
-import { Button, Drawer, Input, Rate, Space } from "antd";
+import { Button, Descriptions, Drawer, Input, Rate, Space } from "antd";
 import { Dispatch, FC, SetStateAction, useState } from "react";
 import { IReview } from "../../../../interfaces/api.products.res.type";
 import { StarFilled } from "@ant-design/icons";
 import TextArea from "antd/es/input/TextArea";
-import { useForm } from "react-hook-form";
+import { Controller, FieldValue, useForm } from "react-hook-form";
 
 interface IEditReviewDrawerProps {
   setReview: Dispatch<SetStateAction<null | IReview>>;
@@ -20,11 +20,13 @@ const EditReviewDrawer: FC<IEditReviewDrawerProps> = ({
   const { handleSubmit, control } = useForm({
     defaultValues: {
       description: review?.description,
-      rating: updatedRating,
     },
   });
 
-  console.log(updatedRating);
+  async function handleUpdateReview(data: any) {
+    console.log(data);
+  }
+
   return (
     <>
       <Drawer
@@ -40,26 +42,37 @@ const EditReviewDrawer: FC<IEditReviewDrawerProps> = ({
         extra={
           <Space>
             <Button onClick={() => setReview(null)}>Cancel</Button>
-            <Button type="primary">Submit</Button>
+            <Button type="primary" onClick={handleSubmit(handleUpdateReview)}>
+              Submit
+            </Button>
           </Space>
         }
       >
-        <div className="flex flex-col items-center gap-4">
-          <TextArea
-            maxLength={200}
-            showCount
-            minLength={5}
-            rows={5}
-            size="large"
-          />
-          <Rate
-            value={updatedRating}
-            onChange={(val) => setUpdatedRating(val)}
-            character={<StarFilled />}
-            className="text-2xl text-red-500"
-          />
-          <p className="text-lg">Current Rating: {review?.rating} stars</p>
-        </div>
+        <form onSubmit={handleSubmit(handleUpdateReview)}>
+          <div className="flex flex-col items-center gap-4">
+            <Controller
+              name="description"
+              control={control}
+              render={({ field }) => (
+                <TextArea
+                  {...field}
+                  maxLength={200}
+                  showCount
+                  minLength={5}
+                  rows={5}
+                  size="large"
+                />
+              )}
+            />
+            <Rate
+              value={updatedRating}
+              onChange={(val) => setUpdatedRating(val)}
+              character={<StarFilled />}
+              className="text-2xl text-red-500"
+            />
+            <p className="text-lg">Current Rating: {review?.rating} stars</p>
+          </div>
+        </form>
       </Drawer>
     </>
   );
