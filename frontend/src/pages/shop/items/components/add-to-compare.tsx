0f8683db52b910e87244f0ecfb04324e5a473d4f
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-12-17 11:53:52 +0600

    add to compare handler added on product details route

--- frontend/src/pages/shop/items/components/add-to-compare.tsx
+++ frontend/src/pages/shop/items/components/add-to-compare.tsx
@@ -1,10 +1,39 @@
 import { Button } from "antd";
 import { FC } from "react";
 import { IProduct } from "../../../../interfaces/api.products.res.type";
+import { useAppDispatch } from "../../../../redux/hooks";
+import {
+  addToCompare,
+  ICompare,
+} from "../../../../redux/features/compare/compare.slice";
+import { toast } from "sonner";
 
-const AddToCompare: FC<IProduct> = () => {
+const AddToCompare: FC<IProduct> = ({ id, categoryId }) => {
+  const dispatch = useAppDispatch();
+
+  const comparePayload: ICompare = {
+    categoryId,
+    productId: id,
+  };
+
+  function handleAddToCompare() {
+    try {
+      dispatch(addToCompare(comparePayload));
+      toast.success("Item added");
+    } catch (error: any) {
+      if (error?.message === "category_conflict") {
+        alert("Category conflict");
+      }
+    }
+  }
+
   return (
-    <Button variant="outlined" size="large" color="danger">
+    <Button
+      onClick={handleAddToCompare}
+      variant="outlined"
+      size="large"
+      color="danger"
+    >
       Compare
     </Button>
   );
