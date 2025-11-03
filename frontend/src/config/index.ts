5f27ce413d91854e443e02b485b3cfe1942195c7
Noothan S <noothanchnr1@gmail.com>
2025-11-03 21:50:50 +0530

    final

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,5 +1,5 @@
 export const config = {
-  server_url: import.meta.env.VITE_API_URL,   
+  server_url: "https://my-backend-app123.azurewebsites.net",
   stripe_publishable_key:
     "pk_test_51OVWkVHfRFCfVZp8dwEwzuDWjUPgnMadf3sHxxb8mf3xzabCF3m5KEDKLHbGOafbOvQFi2NlokV0TBoWAqDaLDt500R0LHclLj",
 };
