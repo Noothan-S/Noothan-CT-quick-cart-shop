3ce2f0b324fc78c44910de27f585a348ac006086
Noothan S <noothanchnr1@gmail.com>
2025-11-04 20:31:06 +0530

    mmmm

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,5 +1,5 @@
 export const config = {
-  server_url: "https://my-backend-app123.azurewebsites.net",
+  server_url: "https://my-backend-app123.azurewebsites.net",   // ✅ Replace with your backend app URL
   stripe_publishable_key:
-    "pk_test_51OVWkVHfRFCfVZp8dwEwzuDWjUPgnMadf3sHxxb8mf3xzabCF3m5KEDKLHbGOafbOvQFi2NlokV0TBoWAqDaLDt500R0LHclLj",
+    "pk_test_51OVWkVHfRFCfVZp8dwEwzuDWjUPgnMadf3sHxxb8mf3xzabCF3m5KEDKLHbGOafbOvQFi2NlokV0TBoWAqDaLDt500R0LHclLj", // ✅ keep your key
 };
